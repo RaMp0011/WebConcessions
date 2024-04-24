a9cb260a01bb9aeec472d5d928dafca3adc59298
Ayush Shukla <128275614+RaMp0011@users.noreply.github.com>
2024-04-24 19:25:13 +0530

    Update ConcForm.js

--- frontend/src/components/ConcForm.js
+++ frontend/src/components/ConcForm.js
@@ -2,21 +2,39 @@ import { useState } from "react";
 import {useConcsContext } from "../hooks/useConcContext";
 import { useAuthContext } from "../hooks/useAuthContext";
 import {motion} from 'framer-motion'
+import {WR,HR,CR} from '../data/stationData'
 
 export default function ConcForm(){
     const {dispatch}=useConcsContext()
-    const [from,setFrom]=useState('')
-    const [via,setVia]=useState('')
+    const [from,setFrom]=useState('Churchgate')
+    const [via,setVia]=useState('Dadar')
     const [clas,setClass]=useState('')
     const [period,setPeriod]=useState('')
     const [error,setError]=useState(null)
+    const [region,setRegion]=useState("WR")
     const[emptyFields,setEmptyFields]=useState([])
     const {user}=useAuthContext()
+   
+    const setCR=()=>{
+        setRegion("CR")
+        setVia("Kurla")
+        setFrom('CSMT')
+    }
+    const setHR=()=>{
+        setRegion("HR")
+        setVia("NA")
+        setFrom('CSMT')
+    }
 
     const handleSubmit=async(e)=>{
-        e.preventDefault()
-
+      
+        e.preventDefault() 
         const conc={from,via,clas,period}
+        const concCheck=from.toUpperCase()
+        if( concCheck==="GOVANDI"){
+            setError("Sorce and destination can't be same")
+            return
+        }
         if(!user){
             setError('You must be logged in')
             return
@@ -49,6 +67,7 @@ export default function ConcForm(){
         if(!response.ok){
             setError(json.error)
             setEmptyFields(json.emptyFields)
+            console.log(json.emptyFields)
         }
         if(response.ok){
             setFrom('')
@@ -65,26 +84,42 @@ export default function ConcForm(){
         <form className="create" onSubmit={handleSubmit}>
         <h2>Apply for Concession</h2>
         <label className="lay">From:</label>
-        <input
+        {/* <input
         type='text'
         onChange={(e)=>setFrom(e.target.value)}
         value={from}
         className={emptyFields.includes('form')?'error':''}
-        />
+        /> */}
+     <div class="radioo2">
+      <input type="radio" id="regionChoic1" name="REGION" value="WR" defaultChecked onChange={(e)=>setRegion(e.target.value)} className={emptyFields.includes('period')?'error':''}/>
+      <label for="regionChoic1">Western</label>
+      <input type="radio" id="regionChoic2" name="REGION" value="CR" onChange={(e)=>setCR(e.target.value)} className={emptyFields.includes('period')?'error':''}/>
+      <label for="regionChoic2">Central</label>
+      <input type="radio" id="regionChoic3" name="REGION" value="HR" onChange={(e)=>setHR(e.target.value)} className={emptyFields.includes('period')?'error':''}/>
+      <label for="regionChoic3">Hourbour</label>
+    </div>
+        <select className="dropbox" name="WR" onChange={(e)=>setFrom(e.target.value)}>
+            {region==="WR"? WR.map((text)=>(
+                <option value={text}>{text}</option>
+            )):region==="CR"?CR.map((text)=>(
+                <option value={text}>{text}</option>
+            )):HR.map((text)=>(
+                <option value={text}>{text}</option>
+            ))}
+        </select>
         <label className="lay">To:</label>
         <input id="default" type="text" value="Govandi" disabled="disabled"></input>
         <label className="lay">via:</label>
-        <input
+        {region==='WR'?<select className="dropbox" name="VIA" onChange={(e)=>setVia(e.target.value)}>
+            <option value={"Dadar"}>Dadar</option>
+            <option value={"Bandra"}>Bandra</option>
+        </select>:region==='CR'? <input
         type='text'
-        onChange={(e)=>setVia(e.target.value)}
-        value={via}
-        className={emptyFields.includes('via')?'error':''}
-        />
-        {/* <label for="clas">Class:</label>
-  <select id="clas" name="class">
-    <option value="First">Second</option>
-    <option value="Second">First</option>
-  </select> */}
+        value="Kurla" disabled="disabled"
+        />:<input
+        type='text'
+        value="NA" disabled="disabled"
+        />}
   <legend>Class:</legend>
     <div class="radioo">
       <input type="radio" id="contactChoice1" name="class" value="Second" onChange={(e)=>setClass(e.target.value) } className={emptyFields.includes('class')?'error':''}/>
